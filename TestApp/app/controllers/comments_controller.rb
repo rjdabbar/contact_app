439cfdc8f241abd5c30204a097031acad2be0dab
Melissa and RJ <melissa+rj@gmail.com>
2015-08-17 17:12:43 -0400

    CRUD Comments

--- TestApp/app/controllers/comments_controller.rb
+++ TestApp/app/controllers/comments_controller.rb
@@ -13,16 +13,41 @@ class CommentsController < ApplicationController
   end
 
   def show
-
+    render json: Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    if @comment
+      Comment.update(comment_params)
+    else
+      render(
+        json: @comment.errors.full_messages, status: :not_found
+      )
+    end
   end
 
   def create
+    @comment = Comment.find(params[:id])
+    if @comment.save!
+      Comment.update(comment_params)
+    else
+      render(
+        json: @comment.errors.full_messages, status: :not_found
+      )
+    end
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    @comment.destroy!
+    render json: @comment
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :commentable_id, :commentable_type)
+  end
+
 end
