50dcaa9c62c0e13e738c862fb90545e7f1b82769
Melissa and RJ <melissa+rj@gmail.com>
2015-08-17 13:52:30 -0400

    Create Controller methods.

--- TestApp/app/controllers/users_controller.rb
+++ TestApp/app/controllers/users_controller.rb
@@ -10,8 +10,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(params[:user].permit(:name, :email))
-    user.save!
-    render json: user
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
   end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def update
+    User.update(user_params)
+  end
+
+  def destroy
+    User.destroy(params[:id])
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email)
+  end
+  
 end
